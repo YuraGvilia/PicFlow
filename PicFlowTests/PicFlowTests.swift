1883ab2e6181bdfabae92aa8336a5cfdeee50810
YuraGvilia <yuragvilia@gmail.com>
2025-03-23 15:50:01 +0400

    Add .gitignore to exclude build, secrets, and user data

---
+++ PicFlowTests/PicFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  PicFlowTests.swift
+//  PicFlowTests
+//
+//  Created by Yura Gvilia on 23.03.2025.
+//
+
+import Testing
+@testable import PicFlow
+
+struct PicFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
